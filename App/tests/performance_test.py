b692f800d9af418fd1f057b6bd7bf361cfe6bb0a
BrandyRumRum <brandon.ramcharitar0529@gmail.com>
2025-04-16 00:30:19 +0000

    Updated performance tests

--- App/tests/performance_test.py
+++ App/tests/performance_test.py
@@ -1,10 +1,31 @@
-import time
-import random
 from locust import HttpUser, task, between
+import random
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        self.client.post("/login", data={
+            "username": "emma",       
+            "password": "emmapass"
+        })
+
+class AuthenticatedStudent(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        self.client.post("/login", data={
+            "username": "mark",      
+            "password": "markpass"
+        })
+
+    
     @task
     def leaderboard_page(self):
         self.client.get(url="/leaderboard")
@@ -20,10 +41,31 @@ class WebsiteUser(HttpUser):
         self.client.get(url=f"/moderator_profile/{moderator_id}")
     
     @task
-    def competitions_page(self):
-        self.client.get(url="/competitions")
+    def all_competitions(self):
+        res = self.client.get("/competitions")
     
     @task
     def competition_details_page(self):
         competition_id = random.randint(1, 6)
         self.client.get(url=f"/competitions/{competition_id}")
+    
+    @task
+    def valid_competitions(self):
+        for comp_id in [1, 2, 3, 4, 5, 6]:
+            self.client.get(f"/competitions/{comp_id}")
+
+    @task
+    def get_notifications(self):
+        self.client.get("/notifications")
+
+    # @task                             
+    # def post_dummy_results(self):
+    #     comp_name = "CodeElite"  # Make sure this exists
+    #     payload = {
+    #         "team_name": f"team{random.randint(100,999)}",
+    #         "student1": "mark",
+    #         "student2": "michelle",
+    #         "student3": "eric",
+    #         "score": random.randint(0, 10)
+    #     }
+    #     self.client.post(f"/add_results/{comp_name}", data=payload)
